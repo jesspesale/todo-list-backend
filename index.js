36fad957686a91d227aa22043caad07ad07adbae
jesspesale <jess_pesale@aol.com>
2022-02-09 21:16:49 -0500

    re-installed npm packages and set up server to run 'npm run dev' wit start it and it uses nodemon

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require('express')
+
+const app = express()
+
+// get takes 2 arguments - 1. the URL 2. callback function
+app.get('/', (req, res) => {
+    res.send("Hey this is Node.js express")
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`A node JS API is listening on port ${port}`)
+})
